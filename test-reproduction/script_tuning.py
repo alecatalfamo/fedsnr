6bc2fb4f3c639b6cbededf2e304ff684c5e65ce5
Alessio FedSNR machine <alessio@fedsbr.com>
2025-10-22 21:22:47 +0000

    test reproduction fix add dataset on test exec script

--- test-reproduction/script_tuning.py
+++ test-reproduction/script_tuning.py
@@ -2,7 +2,8 @@ import json
 import sys
 import argparse
 import random
-def modify_json(input_file, output_file, fitFraction, strategy, distribution, percentage_noisy_clients, fit_clients, seed):
+def modify_json(input_file, output_file, fitFraction, strategy, distribution, dataset,
+                percentage_noisy_clients, fit_clients, seed):
     print("Input file:", input_file)
     with open(input_file, 'r') as f:
         data = json.load(f)
@@ -16,6 +17,7 @@ def modify_json(input_file, output_file, fitFraction, strategy, distribution, pe
     data['strategy'] = strategy
     data['distribution'] = distribution
     data['fitClients'] = fit_clients
+    data['dataset'] = dataset
     
     list_noise_clients = rng.sample(range(0, data['fitClients']), int(data['fitClients'] * percentage_noisy_clients))
     data['noisyClients'] = list_noise_clients
@@ -37,9 +39,12 @@ if __name__ == "__main__":
     argparser.add_argument('--percentage_noisy_clients', type=float, required=True)
     argparser.add_argument('--seed', type=int, required=True)
     argparser.add_argument('--fit_clients', type=int, required=True)
+    argparser.add_argument('--dataset', type=str, required=False, default='alzheimer')
     
     args = argparser.parse_args()
     input_file = args.input
     output_file = args.output
     
-    modify_json(input_file, output_file, args.fitFraction, args.strategy, args.distribution, args.percentage_noisy_clients, args.fit_clients, args.seed)
+    modify_json(input_file, output_file, args.fitFraction, args.strategy, 
+                args.distribution, args.dataset, args.percentage_noisy_clients, 
+                args.fit_clients, args.seed)
--- test-reproduction/test_exec.sh
+++ test-reproduction/test_exec.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 seed=$1
 fit_clients=$2
+dataset=$3
 #percentages=0
 # fitFraction=0.7
 # strategy=FedMRI
@@ -9,12 +10,12 @@ fit_clients=$2
 
 cd ..
 for fitFraction in 0.1 0.3 0.5 0.7; do
-    for strategy in FedAvg FedMRI; do
+    for strategy in FedAvg FedSNR; do
         for distribution in iid dirichlet; do
             for percentages in 0.0 0.2 0.5; do
                 
                     python3 test-reproduction/script_tuning.py --input fedmriapp/fl_config.json --output fedmriapp/fl_config.json \
-                    --fitFraction $fitFraction --strategy $strategy --distribution $distribution \
+                    --fitFraction $fitFraction --strategy $strategy --distribution $distribution --dataset $dataset \
                     --percentage_noisy_clients $percentages --fit_clients $fit_clients --seed $seed;
 
                     flwr run;
